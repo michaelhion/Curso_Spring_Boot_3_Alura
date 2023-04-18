4bf398be5c9f52ed5b52f9c3b5d77c89e72d42fd
Michael Hion <michael.hion1|@gmail.com>
2023-04-18 13:41:26 -0300

    Geração de token concluída

--- src/main/java/med/voll/api/controllers/AutenticacaoController.java
+++ src/main/java/med/voll/api/controllers/AutenticacaoController.java
@@ -3,6 +3,7 @@ package med.voll.api.controllers;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
 import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJwt;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +26,9 @@ public class AutenticacaoController {
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
-        var token = new UsernamePasswordAuthenticationToken(dados.login(),dados.senha());
-        var authentication = manager.authenticate(token);
-
-        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
+        var authenticationtoken = new UsernamePasswordAuthenticationToken(dados.login(),dados.senha());
+        var authentication = manager.authenticate(authenticationtoken);
+        var tokenJwt = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJwt(tokenJwt));
     }
 }
